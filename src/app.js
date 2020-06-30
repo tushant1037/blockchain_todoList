48ae97d670657a9430a974efc29ecc7f7336f2c7
tushant1037 <42100482+tushant1037@users.noreply.github.com>
2020-06-30 20:20:09 +0530

    2. List tasks

---
+++ src/app.js
@@ -0,0 +1,117 @@
+App = {
+    loading: false,
+    contracts: {},
+    load: async () => {
+        //load app
+        await App.loadWeb3()
+        await App.loadAccount()
+        await App.loadContract()
+        await App.render()
+    },
+    loadWeb3: async () => {
+        if(typeof web3 !== 'undefined') {
+            App.web3Provider = web3.currentProvider
+            web3 = new Web3(web3.currentProvider)
+        }
+        else{
+            window.alert("please connect to Metamask.")
+        }
+
+        if (window.ethereum) {
+            window.web3 = new Web3(ethereum)
+            try {
+                await ethereum.enable()
+                web3.eth.sendTransaction({  })
+            }
+            catch (error) {
+
+            }
+        }
+
+        else if (window.web3) {
+            ApplicationCache.web3Provider = web3.currentProvider
+            window.web3 = new Web3(web3.currentProvider)
+
+            web3.eth.sendTransaction({  })
+        }
+
+        else{
+            console.log("Non-Ethereum browser detected. You should try metamask")
+        }
+    },
+
+    loadAccount: async () => {
+        App.account = web3.eth.accounts[0]
+        console.log(App.account)
+    },
+
+    loadContract: async () => {
+        const todoList = await $.getJSON('TodoList.json')
+        App.contracts.TodoList = TruffleContract(todoList)
+        App.contracts.TodoList.setProvider(App.web3Provider)
+        App.todoList = await App.contracts.TodoList.deployed()
+        //console.log(todoList);
+    },
+
+    render: async () => {
+
+        if (App.loading) {
+            return
+        }
+
+        App.setLoading(true)
+
+        $('#account').html(App.account)
+
+        await App.renderTasks()
+
+        App.setLoading(false)
+    },
+
+    renderTasks: async () => {
+        const taskCount = await App.todoList.taskCount()
+        const $taskTemplate = $('.taskTemplate')
+
+        for (var i = 1; i <= taskCount; i++) {
+            const task = await App.todoList.tasks(i)
+            const taskId = task[0].toNumber()
+            const taskContent = task[1]
+            const taskCompleted = task[2]
+
+            const $newTaskTemplate = $taskTemplate.clone()
+            $newTaskTemplate.find('.content').html(taskContent)
+            $newTaskTemplate.find('input')
+                            .prop('name', taskId)
+                            .prop('checked', taskCompleted)
+                            //.on('click', App.toggleCompleted)
+
+        if (taskCompleted) {
+            $('#completedTaskList').append($newTaskTemplate)
+        }
+        else {
+            $('#taskList').append($newTaskTemplate)
+        }
+
+        $newTaskTemplate.show()
+        }
+    },
+
+    setLoading: (boolean) => {
+        App.loading = boolean
+        const loader = $('#loader')
+        const content = $('#content')
+        if (boolean) {
+            loader.show()
+            content.hide()
+        }
+        else{
+            loader.hide()
+            content.show()
+        }
+    }
+}
+$(() => {
+    $(window).load(() => {
+        App.load()
+    })
+})
